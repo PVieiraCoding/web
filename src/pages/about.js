e80de062bcc778f035c21b881ab7b65dba571871
Pedro Vieira <160154461+PVieiraCoding@users.noreply.github.com>
2024-02-20 11:02:14 +0000

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -117,7 +117,7 @@ export default function About() {
               </div>
             </div>
           </div>
-                  <Skills />
+          <Skills />
           <Experience />
           <Education />
         </Layout>
