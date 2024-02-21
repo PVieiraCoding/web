6d34857d21f0a2e3f4db9a1cdc22dabce5a8ce90
Pedro Vieira <160154461+PVieiraCoding@users.noreply.github.com>
2024-02-21 12:03:55 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -147,7 +147,7 @@ const Navbar = () => {
       <nav className="flex items-center justify-center flex-col">
         <CustomMobileLink toggle={handleClick} className="mr-4 lg:m-0 lg:my-2" href="/" title="Home" />
         <CustomMobileLink toggle={handleClick} className="mx-4 lg:m-0 lg:my-2" href="/about" title="About" />
-        <CustomMobileLink toggle={handleClick} className="mx-4 lg:m-0 lg:my-2" href="/projects" title="Projects" />
+        <CustomMobileLink toggle={handleClick} className="mx-4 lg:m-0 lg:my-2" href="https://www.facebook.com/PVieiraIT/" target="_blank" title="Software & Hardware Repair" />
       </nav>
       <nav
         className="flex items-center justify-center  mt-2
